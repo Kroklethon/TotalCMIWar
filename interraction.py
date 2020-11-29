2741837a4856e9c4a65f891767675ebbdb113982
NARWALSMAN <55153254+NARWALSMAN@users.noreply.github.com>
2020-11-29 07:25:29 +0100

    version 1 modifier
    cette version  est une refonte de la version, il manque volontairement la génération de map
    cette version a pour but de retravailler entièrement le code pour l'interaction et la gestion de personnage/environnement/gameplay
    cette version est maintenant capable de géré plus de deux entité passant a n entité accepté

--- interraction.py
+++ interraction.py
@@ -1,69 +1,94 @@
-def positionj(tableau_jeu, statueobj):
-	'''
-	entrer:tableau a 2 dimention de cdictionnaire, rpz la grille du jeu
-		   statue de la case (choix de ce qu'on veut chercher) que l'on cherche en coordonner
-	sortie: tableau a 2 case 0 position x et 1 position y
-	'''
+import pygame
+import ModulePersonne
+mode_selection=False
+personnage_a_action=0
 
-	position = []
-	trouver = False
-	indice = 0
-
-	while trouver == False and indice < len(tableau_jeu):
-		indice2 = 0
-		while trouver == False and indice2 <len(tableau_jeu[indice]):
-			if tableau_jeu[indice][indice2]['statue'] == statueobj:
-				trouver = True
-				prosition = position + [indice]
-				prosition = position + [indice2]
-
-	return position
-	
-def isoverj (tableau_jeu, position, joueur):
-        '''
-        entrer:	position d'un objet type position de pygame
-                        tableau a 2 dimention de cdictionnaire, rpz la grille du jeu
-                        joueur entier qui rpz soit le joueur 1 ou 2 
-        sortie: boolean qui verifie si l'objet(position) est bien sur le joueur 1
-
-        '''
-        estaudessus = False
-        if joueur == 1:
-                posjoueur = positionj(tableau_jeu, 'joueur_1')
-        if joueur == 2:
-                posjoueur = positionj(tableau_jeu, 'joueur_2')
+#deplacer_combat#############################################################################################################################
+def deplacer_combat(tour):
+        """
+        entrée/sortie:
+                entree: boolean -> definie le tour du joueur (qui joue)
+        
+        fonction de base qui vas géré les déplacement et le combat
+        """
+        liste_tour=liste_joueur_jouable(tour)
+        pos = pygame.mouse.get_pos()
+        perso = selection_sourie(pos)
+        if mode_selection == True:
+                interraction_selection(liste_tour,perso)
+        else:
+                cree_selection(liste_tour,perso)
+#interraction_selection###########################################
+def interraction_selection(liste_tour,perso,personnage_a_action):
+        """
+        entrée/sortie:
+                entree: boolean -> la liste des nom des joueur jouable
+                        personnage -> personnage a traiter
+                        personnage -> personnage en attente d'instruction
+        fonction de base qui vas géré les déplacement et le combat 
+        """
+        if perso[0] != "vide" and perso[0] != "montagne":       #si on click sur un personnage
+                attaquer(perso[1],personnage_a_action)             #test attaque
+        elif perso[0] == "vide":                                #si on click sur une case vide
+                deplacer(perso[1],personnage_a_action)             #test deplacer
+                                                                #si on click sur une case infranchissable
+        mode_selection = False
+#cree_selection##########################################
+def cree_selection(liste_tour,perso):
+        """
+        entrée/sortie:
+                entree: liste -> la liste des nom des joueur jouable
+                personnage -> personnage a traiter
                 
-        if pos[0] > posjoueur[0] and pos[0] < posjoueur[0] + width:
-                if pos[1] > posjoueur[1] and pos[1] < posjoueur[1] +height:
-                        estaudessus = True
-        return estaudessus
+        fonction qui met en mode selection et instancie quel personnage a utiliser
+        """
+        if perso[0] != "vide" and perso[0] != "montagne":       #si on a bien cliquer sur un personnage
+                if perso[1].nom in liste_tour:                  #si le nom est dans la lsite ( a son tour de jouer)
+                        mode_selection=True                     
+                        personnage_a_action=perso
+#liste_joueur_jouable###########################################
+def liste_joueur_jouable(tour):
+        """
+        entrée/sortie:
+                entree: boolean -> definie le tour du joueur (qui joue)
+                sorite: liste -> retourne la liste des nom des joueur jouable
+        fonction qui renvois la liste de spersonnage jouable
+        """
+        liste=[]
+        if tour == True:
+                liste=["hugo","flo","evan"]
+        else:
+                liste=["aurelien","julien","lea"]
+        return liste
+#deplacer_combat#############################################################################################################################       
 
-def isovertt (tableau_jeu, position):
-	'''
-	entrer:	position d'un objet type position de pygame
-			tableau a 2 dimention de cdictionnaire, rpz la grille du jeu
-	sortie: boolean qui verifie si l'objet(position) est bien sur n'importequoi
 
-	 
-	'''
-	estaudessus = False
-	if tableau_jeu[int(pos[0])][int(pos[1])].statue == "vide":
-                estaudessus = True
-	return estaudessus
 
-def setposj1 (tableau_jeu, position, joueur):
-	'''
-	entrer: position d'un objet type position de pygame
-			tableau a 2 dimention de cdictionnaire, rpz la grille du jeu
-			joueur entier qui rpz soit le joueur 1 ou 2 
-	sortie: rien
-	
-	'''
-	if joueur == 1:
-		posjoueur = positionj(tableau_jeu, joueur_1)
-		tableau_jeu[position[0]][position[1]].statue = "joueur_1" 
-	if joueur == 2:
-		posjoueur = positionj(tableau_jeu, joueur_2)
-		tableau_jeu[position[0]][position[1]].statue = "joueur_2" 
 
-	tableau_jeu[posjoueur[0]][posjoueur[1]].statue = "vide" 
+#interraction_selection######################################################################################################################
+
+#deplacer######################################################################################################################
+def deplacer(tableau,perso):
+        """
+        entree/sortie:
+                entree: personnage -> personnage a déplacer
+                        tableau -> position a aller
+        fonction qui gere le déplacement
+        """
+        #copier coller de l'autre fichier
+#combatre######################################################################################################################
+def attaquer(defenseur,attaquant):
+        """
+        entree/sortie:
+                entree: personnage -> defenseur qui se fait attaquer
+                        personnage-> personnage qui attaque
+        fonction qui gère le combat 
+        """
+        #copier coller de l'autre fichier
+#interraction_selection######################################################################################################################
+
+def selection_sourie(pos):
+        #faire selection sourie
+        #copier coller de l'autre fichier
+        tablo=["vide",5,5]
+        return tablo
--- main.py
+++ main.py
@@ -1,139 +1,80 @@
 ###################################importation###############################################################
 import pygame
 import main_var
-import sys 
-import boutton
+import initialisation
 import interraction
+import boutton
+import ModulePersonne
 import functions
 ###################################importation###############################################################
+
 
 
 ###################################def variable##############################################################
 var_pour_gen = 1
 var_lance_jeu = True
-
-sys.path.insert(var_pour_gen, '/gen_terrain')
 pygame.init()
-seed = functions.get_seed()
-t,shape = functions.create_noise_map(seed)
-functions.get_map_image(seed)
 screen = pygame.display.set_mode(main_var.size)
-
 background = pygame.image.load("carte_couleur.png")
+tab = initialisation.cree_table()
 ###################################def variable##############################################################
 
+###################################def personnage##############################################################
+evan=ModulePersonne.Personne("evan")
+tab[evan.posx][evan.posy]="evan"
+aurelien=ModulePersonne.Personne("aurelien")
+tab[aurelien.posx][aurelien.posy]="aurelien"
+###################################def personnage##############################################################
 
-###################################code def jeu##############################################################
-def creecase(var_pour_gen, posx, posy,tableau, rectangle):
+print(tab)
+###################################cree tableau##############################################################            
+def creecase(var_pour_gen, posx, posy,tableau,rect):
 	case_tabl = tableau[posx][posy]
-	rect=rectangle
-	if case_tabl['statue'] == 'montagne' :
-		pygame.draw.rect(screen, main_var.montagne, rect, var_pour_gen)
-	elif case_tabl['statue'] == 'joueur_1':
-		pygame.draw.rect(screen, main_var.joueur_1, rect, var_pour_gen)
-	elif case_tabl['statue'] == 'joueur_2':
-		pygame.draw.rect(screen, main_var.joueur_2, rect, var_pour_gen)
+	if case_tabl == 'montagne' :
+		pygame.draw.rect(screen, main_var.montagne, rect, 0)
+	elif case_tabl == 'evan':
+		pygame.draw.rect(screen, main_var.joueur_1, rect, 0)
+	elif case_tabl == 'aurelien':
+		pygame.draw.rect(screen, main_var.joueur_2, rect, 0)
 	else:
 		pygame.draw.rect(screen, main_var.white, rect, var_pour_gen)
-
-def cree_table():
-    """
-    entree: rien
-    sortie: rien 
-
-    crée le tableau vide correspondant au case du tableau
-    """
-    tailletab = 30
-    tailleligne = 30
-    #####cration variable ######
-    tableau = []                   #TABLEAU ->
-    dico = {}                      #DICTIONNAIRE 
-    dico['statue'] = 'vide'        #statue vide (case vide)
-    #####cration variable ######
-    tableau = [[dico] * tailleligne] * tailletab
-    return tableau 
-
 def drawGrid():
     blockSize = 30 #Set the size of the grid block
     for x in range(blockSize):
         for y in range(blockSize):
             rect = pygame.Rect(x*blockSize, y*blockSize, blockSize, blockSize)
-            pygame.draw.rect(screen, main_var.white, rect, var_pour_gen)
+            creecase(var_pour_gen,x,y,tab,rect)
+            
 
-###################################code def jeu##############################################################
+###################################cree tableau##############################################################
+
+
+###################################cree boutton##############################################################
+boutton_passer_tour=boutton.button((255,1,1),900,700,300,50,"passer tour")
+###################################cree boutton##############################################################
 
 
 
 ###################################lanceur de jeu############################################################
-tableau_jeu = cree_table()
-######################ici on crée des perso et une montagne#################
-###joueur 1
-tableau_jeu[3][6]['statue'] = 'joueur_1'
-###joueur 2         
-tableau_jeu[4][5]['statue'] = 'joueur_2'
-print (tableau_jeu[4][5])
-print(tableau_jeu[1][6])
-######################provisoire pour la v1#################################
-#bouton de test qui serivra plus tard a passer son tour#########################
-#boutonfintour = boutton.button((0,255,0), 600, 600, 200, 80, 'passer son tour')#
-#boutonfintour.draw(screen ,(0,0,0))                                            #
-#bouton de test qui serivra plus tard a passer son tour#########################
-tourjoueur1 = True #variable qui definie qui peut jouer ici c'est au tour du joueur 1
-dragj1 = False
-dragj2 = False
+tour = True #variable qui definie qui peut jouer ici c'est au tour du joueur 1
 while var_lance_jeu == True:
+    boutton_passer_tour.draw(screen, (1,200,0)) #dessine bouton fin de tour
     for event in pygame.event.get():
-
-        if event.type == pygame.QUIT: 
-            sys.exit()
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            position = pygame.mouse.get_pos()
-            pos_grid = functions.get_pos_grid(position)
-            #print(get_height_case(pos_grid,t))
-            print(pos_grid)
-
-        #######################test pour le passer son tour#####################
-
-        #if event.type == pygame.MOUSEBUTTONDOWN: # test si on clique sur le bouton et passe le tour
-            #if boutonfintour.isover(position):
-            #    if tourjoueur1 == True:
-            #        tourjoueur1 = False
-            #    else:
-            #        tourjoueur1 = True
-            #    print("tour passer")#provisoire
-
-        #if event.type == pygame.MOUSEMOTION: # test si la sourie est dessus le bouton et change la couleur
-            #if boutonfintour.isover(position):
-            #    boutonfintour.color = (255,255,255)
-            #else:
-            #    boutonfintour.color = (255,0,0)
-
-        #######################test pour le passer son tour#####################
-
-        #################zone de test du jeu####################################
-
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            #test si on clic sur le joueur souhaiter et passe en mode drag and drop
-            if interraction.isoverj(tableau_jeu, position, 1) and tourjoueur1 == True:
-                dragj1 = True
-            if interraction.isoverj(tableau_jeu, position, 2) and tourjoueur1 == False:
-                dragj2 = True
-            # si en mode drag and drop et click dans zone libre 
-            if dragj1 == True and interraction.isovertt(tableau_jeu, position) == False:
-                interraction.setposj(tableau_jeu, position, 1)
-                dragj1 = False
-            if dragj2 == True and interraction.isovertt(tableau_jeu, position) == False:
-                interraction.setposj(tableau_jeu, position, 2)
-                dragj2 = False
-
-        #################zone de test du jeu####################################
-
+        if event.type == pygame.QUIT:
+            var_lance_jeu = False 
+            pygame.quit()
+    #info_jeu#
+        position_sourie = pygame.mouse.get_pos()
+        if event.type == pygame.MOUSEBUTTONDOWN:        #quand le joeur clique
+            interraction.deplacer_combat(tour)          #programme qui gere le placecement et le combat
+            if boutton_passer_tour.isOver(position_sourie):
+                tour=boutton.interaction_fin_de_tour(tour)      #programme qui gere l'interaction fin de tour
+    #info-jeu#
+    if boutton_passer_tour.isOver(position_sourie):
+        boutton_passer_tour.draw(screen, (1,100,100)) #dessine bouton fin de tour
     screen.blit(background,(0,0))
-    
-
-    drawGrid()   
+    drawGrid()
     pygame.display.update()
 
 ###################################lanceur de jeu############################################################
-sys.exit()
 
