55b4196671ec03271e56dc902b6633177d07c5b8
NARWALSMAN <55153254+NARWALSMAN@users.noreply.github.com>
2020-12-19 18:28:51 +0100

    menu choix
    code fini manque les test (marche surment pas)

--- menuchoix.py
+++ menuchoix.py
@@ -5,7 +5,7 @@ import button
 continuer=True
 def menujeu():
 
-
+	stop=True
 	ecran = pygame.display.set_mode((900, 900))
 	background = pygame.image.load("img/fondmenu.png")
 
@@ -14,39 +14,71 @@ def menujeu():
 	boutonDj1=pygame.image.load("img/boutonselec.png")
 	boutonGj2=pygame.image.load("img/boutonselecgauche.png")
 	boutonDj2=pygame.image.load("img/boutonselec.png")
+
+	boutonGjover1=pygame.image.load("img/boutonselecgaucheover.png")
+	boutonDjover1=pygame.image.load("img/boutonselecover.png")
+	boutonGjover2=pygame.image.load("img/boutonselecgaucheover.png")
+	boutonDjover2=pygame.image.load("img/boutonselecover.png")
 	#bouton des fleche de selection
 
 	#bouton lancer
 	boutonlancer=pygame.image.load("img/lancer.png")
 	#bouton lancer
-	ecran.blit(background,(0,0))
 
+	#image
+	info = pygame.image.load("info.png")
+	math = pygame.image.load("math.png")
+	#image
+	ecran.blit(background,(0,0))
+	ecran.blit(info, (550,400))
+	ecran.blit(math, (550,700))
 
 	while continuer:
 		Joueur1 = joueur()
 		Joueur2 = joueur()
 		pygame.display.flip()
 		##################################
+		screen.blit(boutonGj1, (500,400))
+		screen.blit(boutonDj1, (800,400))
+		screen.blit(boutonGj2, (500,700))
+		screen.blit(boutonDj2, (800,700))
+		##################################
 		for event in pygame.event.get():
         ###
         if event.type == pygame.QUIT:
             continuer = False
+            stop=False
         ###
         position_souris = pygame.mouse.get_pos()
         if event.type == pygame.MOUSEBUTTONDOWN:
         	###
-        	if bouttonGj1.isOver(position_souris):
+        	if boutonGj1.isOver(position_souris):
         		Joueur1.createClasse("info")
+        		ecran.blit(info, (550,400))
         	###
-        	if bouttonDj1.isOver(position_souris):
+        	if boutonDj1.isOver(position_souris):
         		Joueur1.createClasse("math")
+        		ecran.blit(math, (550,400))
         	###
-        	if bouttonGj2.isOver(position_souris):
+        	if boutonGj2.isOver(position_souris):
         		Joueur2.createClasse("info")
+        		ecran.blit(info, (550,700))
         	###
-        	if bouttonDj2.isOver(position_souris):
+        	if boutonDj2.isOver(position_souris):
         		Joueur2.createClasse("math")
+        		ecran.blit(math, (550,700))
+        	if lancer.isOver(position_souris):
+        		continuer=False
         ################################## 
+        if boutonGj1.isOver(position_souris):
+        	screen.blit(boutonGj1, (500,400))
+        if boutonDj1.isOver(position_souris):
+        	screen.blit(boutonDj1, (800,400))
+        if boutonGj2.isOver(position_souris):
+        	screen.blit(boutonGj2, (500,700))
+        if boutonDj2.isOver(position_souris):
+        	screen.blit(boutonDj2, (800,700))
+
 	pygame.quit()
 	joueurs=[Joueur1,Joueur2]
-	return joueurs
+	return joueurs,stop
